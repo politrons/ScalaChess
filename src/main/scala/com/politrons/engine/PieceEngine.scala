dfd67c85b07a7b755f61f771dbcf65f192837fff
politrons <politrons@gmail.com>
2021-08-14 14:28:16 +0200

    refactor inCheck function

--- src/main/scala/com/politrons/engine/PieceEngine.scala
+++ src/main/scala/com/politrons/engine/PieceEngine.scala
@@ -1,8 +1,8 @@
 package com.politrons.engine
 
 import com.politrons.exceptions.IllegalMovementException
-import com.politrons.model.ChessDomain
-import com.politrons.model.ChessDomain.{ColumnFrom, ColumnTo, Movement, Player1, Player2, RowFrom, RowTo}
+import com.politrons.model.{ChessDomain, Piece}
+import com.politrons.model.ChessDomain.{ColumnFrom, ColumnTo, Movement, Player, Player1, Player2, RowFrom, RowTo}
 import com.politrons.view.ChessBoard
 
 import scala.util.{Success, Try}
@@ -24,33 +24,9 @@ trait PieceEngine {
     val triedUnit = Try {
       movement.player match {
         case Player1() =>
-          for (row <- 0 to 7) {
-            for (column <- 0 to 7) {
-              ChessBoard.board(row)(column) match {
-                case Some(piece) if piece.player == Player2() =>
-                  val opponentMovement = Movement(Player2(), 0, ColumnFrom(0), RowFrom(0), ColumnTo(column), RowTo(row))
-                  piece.isCheck(opponentMovement) match {
-                    case Success(isCheck) if isCheck => throw IllegalMovementException(s"Illegal move of ${maybePieceFrom.get.name}, since it let the King in Check by $movement")
-                    case _ => ()
-                  }
-                case _ => ()
-              }
-            }
-          }
+          checkOpponentPieceForCheck(movement, Player2(), maybePieceFrom)
         case Player2() =>
-          for (row <- 0 to 7) {
-            for (column <- 0 to 7) {
-              ChessBoard.board(row)(column) match {
-                case Some(piece) if piece.player == Player1() =>
-                  val opponentMovement = Movement(Player1(), 0, ColumnFrom(0), RowFrom(0), ColumnTo(column), RowTo(row))
-                  piece.isCheck(opponentMovement) match {
-                    case Success(isCheck) if isCheck => throw IllegalMovementException(s"Illegal move of ${maybePieceFrom.get.name}, since it let the King in Check by $movement")
-                    case _ => ()
-                  }
-                case _ => ()
-              }
-            }
-          }
+          checkOpponentPieceForCheck(movement, Player1(), maybePieceFrom)
       }
     }
     ChessBoard.board(movement.rowFrom.value)(movement.columnFrom.value) = maybePieceFrom
@@ -67,5 +43,23 @@ trait PieceEngine {
     val vertical = Math.abs(movement.rowFrom.value - movement.rowTo.value)
     val horizontal = Math.abs(movement.columnFrom.value - movement.columnTo.value)
     (vertical, horizontal)
+  }
+
+  private def checkOpponentPieceForCheck(movement: Movement,
+                                         opponentPlayer:Player,
+                                         maybePieceFrom: Option[Piece]): Unit = {
+    for (row <- 0 to 7) {
+      for (column <- 0 to 7) {
+        ChessBoard.board(row)(column) match {
+          case Some(piece) if piece.player == opponentPlayer =>
+            val opponentMovement = Movement(opponentPlayer, 0, ColumnFrom(0), RowFrom(0), ColumnTo(column), RowTo(row))
+            piece.isCheck(opponentMovement) match {
+              case Success(isCheck) if isCheck => throw IllegalMovementException(s"Illegal move of ${maybePieceFrom.get.name}, since it let the King in Check by $movement")
+              case _ => ()
+            }
+          case _ => ()
+        }
+      }
+    }
   }
 }
